c70bfd25c1afb81921e8bcbadac27fab4b99dd08
Sushant Chandra Kalia <sushantkalia0610@gmail.com>
2023-05-27 07:59:28 -0700

    terraform-settings-providers-resources

---
+++ terraform-settings-providers-resources/c2-ec2instance.tf
@@ -0,0 +1,9 @@
+# Resource: EC2 Instance
+resource "aws_instance" "myec2vm" {
+  ami = "ami-0715c1897453cabd1"
+  instance_type = "t3.micro"
+  user_data = file("${path.module}/app1-install.sh")
+  tags = {
+    "Name" = "EC2 Demo"
+  }
+}
