755f4b1873cf57b084bd92e7fd37127afb8a1501
Sushant Chandra Kalia <sushantkalia0610@gmail.com>
2023-06-05 12:32:03 -0700

    terraform-variables-and-datasources

---
+++ terraform-variables-and-datasources/c1-versions.tf
@@ -0,0 +1,16 @@
+# Terraform Block
+terraform {
+  required_version = "~> 1.4"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.47"
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  region  = var.aws_region
+  profile = "default"
+}
