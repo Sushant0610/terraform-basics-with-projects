5a4ad156c97bcecd131e19f21a90f8f5a3fce263
Sushant Chandra Kalia <sushantkalia0610@gmail.com>
2023-06-16 03:07:34 -0700

    cleanup

--- terraform-settings-providers-resources/c2-ec2-instance.tf
+++
@@ -1,9 +0,0 @@
-# Resource: EC2 Instance
-resource "aws_instance" "myec2vm" {
-  ami = "ami-04a0ae173da5807d3"
-  instance_type = "t3.micro"
-  user_data = file("${path.module}/app1-install.sh")
-  tags = {
-    "Name" = "EC2-Demo"
-  }
-}
